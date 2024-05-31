7167104aa7afdd9ddeb9516b3ec77ddc901c12ac
Ganeshvedhachalam <ganeshgreat08@gmail.com>
2024-06-01 00:45:01 +0530

    finished

---
+++ src/Spell.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+function SpellCheckApp() {
+    const customDictionary = {
+        teh: "the",
+        wrok: "work",
+        fot: "for",
+        exampl: "example",
+      };
+      
+    const[inputText,setInputText]=useState("");
+    const[suggestedText,setSuggestedText]=useState("")
+
+    const handleInputChange=(e)=>{
+        const text=e.target.value
+        setInputText(text)
+        const words=text.splt(" ")
+        const correctedwords= words.map((word)=>{
+            const correctedword=customDictionary[word.toLowerCase()]
+            return correctedword || word 
+        })
+        const correctedText = correctedwords.join(" ");
+
+
+        const firstcorrection =correctedwords.find((word,index) =>word!==words[index])
+        setSuggestedText(firstcorrection||"")
+ 
+    }
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>Spell Check and Auto-Correction</h1>
+        <textarea
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Enter text..."
+          rows={5}
+          cols={40}
+        />
+        </header>
+        {suggestedText&&
+        <p>
+        Did you mean: <strong>suggestedText</strong>?
+      </p>
+      }
+      </div>
+
+      
+    );
+  }
+  
+  export default SpellCheckApp;
+  
